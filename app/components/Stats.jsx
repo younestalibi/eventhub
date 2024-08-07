beb0b5ed92576498c25832b84ae110996a723d5f
Younes <younessetalibi11@gmail.com>
2024-08-07 20:47:18 +0100

    update

--- app/components/Stats.jsx
+++ app/components/Stats.jsx
@@ -1,7 +1,4 @@
-"use client"
-
-import React from 'react';
-import CountUp from 'react-countup';
+import React from 'react'
 
 const Stats = () => {
     return (
@@ -9,36 +6,37 @@ const Stats = () => {
             <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
                 <div className="mx-auto max-w-3xl text-center">
                     <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">Trusted by eCommerce Businesses</h2>
+
                     <p className="mt-4 text-gray-500 sm:text-xl">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
                         provident impedit esse recusandae facere libero harum sequi.
                     </p>
                 </div>
+
                 <div className="mt-8 sm:mt-12">
                     <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:divide-x sm:divide-gray-100">
                         <div className="flex flex-col px-4 py-8 text-center">
                             <dt className="order-last text-lg font-medium text-gray-500">Total Sales</dt>
-                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
-                                <CountUp end={4800000} duration={2.5} separator="," />
-                            </dd>
+
+                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">$4.8m</dd>
                         </div>
+
                         <div className="flex flex-col px-4 py-8 text-center">
                             <dt className="order-last text-lg font-medium text-gray-500">Official Addons</dt>
-                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
-                                <CountUp end={24} duration={2.5} />
-                            </dd>
+
+                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">24</dd>
                         </div>
+
                         <div className="flex flex-col px-4 py-8 text-center">
                             <dt className="order-last text-lg font-medium text-gray-500">Total Addons</dt>
-                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
-                                <CountUp end={86} duration={2.5} />
-                            </dd>
+
+                            <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">86</dd>
                         </div>
                     </dl>
                 </div>
             </div>
         </section>
-    );
-};
+    )
+}
 
-export default Stats;
+export default Stats
--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
+import Pricing from "./components/Pricing";
+import AboutUs from "./components/AboutUs";
 
 export default function Home() {
   return (
     <div className="mb-10">
       <Hero />
+      <AboutUs/>
       <Stats/>
+      <Pricing/>
     </div>
   );
 }
