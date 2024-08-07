ac22cb9670bf76d43cb811096f94829e3712cf15
Younes <younessetalibi11@gmail.com>
2024-08-07 21:38:45 +0100

    pricing

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 import Hero from "./components/Hero";
-// import Stats from "./components/Stats";
-// import Pricing from "./components/Pricing";
-// import AboutUs from "./components/AboutUs";
+import Stats from "./components/Stats";
+import Pricing from "./components/Pricing";
+import AboutUs from "./components/AboutUs";
 
 export default function Home() {
   return (
     <div className="mb-10">
       <Hero />
-      {/* <AboutUs/>
+      <AboutUs/>
       <Stats/>
-      <Pricing/> */}
+      <Pricing/>
     </div>
   );
 }
