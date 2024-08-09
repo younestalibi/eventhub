852102f77701a59a01fb1e245ceaa6394b107cb2
Younes <younessetalibi11@gmail.com>
2024-08-09 12:02:53 +0100

    update event date

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Speakers from "./components/Speakers";
 import Sponsors from "./components/Sponsors";
 
 export default function Home() {
-  const eventDate = '2024-12-31T00:00:00';
+  const eventDate = '2024-09-30T17:30:00+04:00';
 
   return (
     <div className="mb-52">
